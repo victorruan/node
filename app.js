cbcfaf88bbdf3875b3057b6754af5b13693323b4
阮佳佳 <ruanjiajia@ruanjiajiadeMacBook-Pro.local>
2017-01-19 16:39:37 +0800

    第二次迭代

--- app.js
+++ app.js
@@ -24,13 +24,46 @@ function combineFiles(pathnames, callback) {
     }(0, pathnames.length));
 }
 
+function outputFiles(pathnames, writer) {
+    (function next(i, len) {
+        if (i < len) {
+            var reader = fs.createReadStream(pathnames[i]);
+
+            reader.pipe(writer, { end: false });
+            reader.on('end', function() {
+                next(i + 1, len);
+            });
+        } else {
+            writer.end();
+        }
+    }(0, pathnames.length));
+}
+
+function validateFiles(pathnames, callback) {
+    (function next(i, len) {
+        if (i < len) {
+            fs.stat(pathnames[i], function (err, stats) {
+                if (err) {
+                    callback(err);
+                } else if (!stats.isFile()) {
+                    callback(new Error());
+                } else {
+                    next(i + 1, len);
+                }
+            });
+        } else {
+            callback(null, pathnames);
+        }
+    }(0, pathnames.length));
+}
+
 function main(argv) {
     var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8')), root = config.root || '.', port = config.port || 80;
 
     http.createServer(function (request, response) {
         var urlInfo = parseURL(root, request.url);
 
-        combineFiles(urlInfo.pathnames, function (err, data) {
+        validateFiles(urlInfo.pathnames, function (err, pathnames) {
             if (err) {
                 response.writeHead(404);
                 response.end(err.message);
@@ -38,7 +71,7 @@ function main(argv) {
                 response.writeHead(200, {
                     'Content-Type': urlInfo.mime
                 });
-                response.end(data);
+                outputFiles(pathnames, response);
             }
         });
     }).listen(port);
@@ -47,12 +80,15 @@ function main(argv) {
 function parseURL(root, url) {
     var base, pathnames, parts;
 
+    //indexOf  如果要检索的字符串值没有出现，则该方法返回 -1。
     if (url.indexOf('??') === -1) {
+        //replace 将第一次出现的/ 换成 /??
         url = url.replace('/', '/??');
     }
 
+    //url : http://127.0.0.1/??google,baidu
     parts = url.split('??');
-    base = parts[0];
+    base = parts[0];//http://127.0.0.1/
     pathnames = parts[1].split(',').map(function (value) {
         return path.join(root, base, value);
     });
